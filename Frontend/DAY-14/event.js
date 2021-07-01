0317bd9d272073c68f86854fa1c6697c1b1d090f
Shashwat Kapoor <shashwatkapoor14@gmail.com>
2021-07-01 17:28:03 +0530

    DAY-14

---
+++ Frontend/DAY-14/event.js
@@ -0,0 +1,38 @@
+function singleclick() {
+    console.log("Single Click occured!");
+}
+function doubleclick() {
+    console.log("Double Click occured!");
+}
+function drag() {
+    console.log("Mouse is being dragged!");
+}
+function mousemove() {
+    console.log("mouse is beingh moved!");
+}
+function mouseout() {
+    console.log("mouse moved out of element!");
+}
+function mouseover() {
+    console.log("mouse moving over the element!");
+}
+function mousedown() {
+    console.log("Mouse clicked  down!");
+}
+function wheel() {
+    console.log("Wheel moving!");
+}
+function mouseUp() {
+    console.log("Mouse clicked up")
+}
+function keydown() {
+    console.log("Key pressed");
+}
+function keyup() {
+    console.log("Key released");
+}
+
+
+
+
+
